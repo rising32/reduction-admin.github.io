a5f80d195d8d4d37b61af65730d77c5552812d34
rising32 <jscrazy@yandex.com>
2018-09-11 13:51:44 +0800

    second page fixed

--- src/components/Layout/SecondLayout.js
+++ src/components/Layout/SecondLayout.js
@@ -1,9 +1,10 @@
-import { Content, Footer } from 'components/Layout';
+import { Content, Header, Footer } from 'components/Layout';
 import React from 'react';
 
 const EmptyLayout = ({ children, ...restProps }) => (
   <main className="cr-app bg-light" {...restProps}>
     <Content fluid onClick={this.handleContentClick}>
+      <Header />
       {children}
       <Footer />
     </Content>
